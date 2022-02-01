0f0702264d2322fc1f2f1843688542b22896611b
Zoya <mizz_z08@hotmail.com>
2022-02-01 15:37:21 +0000

    complete rock paper scissors game via command line

--- simple_game.py
+++ simple_game.py
@@ -3,44 +3,46 @@
 num_list = [5, 7, 8, 9, 22, 55, 99, 100000]
 
 
+from asyncore import loop
 import random
 
 print(random.choice(num_list)) #will print a random number from list
 
-#num = input("Pick a number ")
-
-#if __name__ =='__main__': #implies module is being run standalone by the user
-
 gesture = ["rock", "paper", "scissors"]
 
-#r = gesture["rock"]
-
-#p = gesture["paper"]
-
-#s = gesture["scissors"]
-
-#pw = p > r
-
-#pl = s > r
 
 win = 0
 loss = 0
+draw = 0
+
 computer_choice = random.choice(gesture)
 
-player_choice = input("Choose rock, paper, or scissors: ")
-print("Computer chose: ", computer_choice, "Player chose: ", player_choice)
-while True:
-    if computer_choice == player_choice:
-        print("It's a draw")
-        #print("try again?")
-        #new_game = input("yes or no")
-                
-    elif computer_choice == "rock" and player_choice == "paper" or computer_choice == "scissors" and player_choice == "rock" or computer_choice == "paper" and player_choice == "scissors":
-        win = win + 1
-        print("Player wins!", "Wins:", win)
-    else:
-        loss = loss + 1
-        print("Computer wins!", " Losses: ", loss)
-    break
 
+player_continue = True
+
+while player_continue:
+    player_choice = input("Choose rock, paper, or scissors: ")
+    
+    if computer_choice == player_choice: 
+        draw = draw + 1 #adds 1 to the overall number of draws
+        print("Computer chose: ", computer_choice, "//", "Player chose: ", player_choice)
+        print("It's a draw", "Wins: ", win, "Losses: ", loss, "Draws: ", draw) #shows stats
+        continue
+    elif computer_choice == "rock" and player_choice == "paper" or computer_choice == "scissors" and player_choice == "rock" or computer_choice == "paper" and player_choice == "scissors":
+        win = win + 1 #adds 1 to the overall number of wins
+        print("Computer chose: ", computer_choice, "//", "Player chose: ", player_choice)
+        print("Player wins!", "Wins: ", win, "Losses: ", loss, "Draws: ", draw) #shows stats
+        continue
+    elif computer_choice == "paper" and player_choice == "rock" or computer_choice == "rock" and player_choice == "scissors" or computer_choice == "scissors" and player_choice == "paper":
+        loss = loss + 1 #adds 1 to the overall number of losses
+        print("Computer chose: ", computer_choice, "//", "Player chose: ", player_choice)
+        print("Computer wins!", "Wins: ", win, " Losses: ", loss, "Draws: ", draw) #shows stats
+        continue
+    else:
+        if player_choice == "x": #press x to quit game
+            player_continue = False #breaks loop
+            print("Game ended\n", "Your Stats: ", "Wins: ", win, "Losses: ", loss, "Draws: ", draw) #confirm loop has broken
+        break
+    
+    
 
